d47cc35a6216a8dff706ac6b2cc1e88b14974d9d
Leon Dietrich <doralitze@chaotikum.org>
2023-01-20 23:08:24 +0100

    add: first draft of artnet output

--- src/dmx/universe.hpp
+++ src/dmx/universe.hpp
@@ -1,18 +1,38 @@
 #pragma once
 
-#include <array>
 #include <cstdint>
+#include <iterator>
+#include <ranges>
 
 #define DMX_UNIVERSE_SIZE 512
 
 namespace dmxfish::dmx {
 
 	typedef uint8_t channel_8bit_t;
+	typedef std::iterator<contiguous_iterator_tag, channel_8bit_t> universe_iterator;
 
-	struct universe {
+	enum class universe_type : uint8_t {
+		PHYSICAL = 0,
+		ARTNET = 1
+	};
+
+	class universe {
+	private:
 		const int id;
-		std::array<channel_8bit_t, DMX_UNIVERSE_SIZE> data;
+		const universe_type type;
+	public:
+		universe(const int id, const universe_type type) : id(id), type(type) {}
 
-		universe(int id) : id(id), data{} {}
+		const int getID() const {
+			return this->id;
+		}
+
+		const universe_type getUniverseType() const {
+			return this->type;
+		}
+
+		virtual channel_8bit_t& operator[](size_t p) = 0;
+		virtual universe_iterator begin() = 0;
+		virtual universe_iterator end() = 0;
 	};
 }
