c6d09272674452a05ed00cc7b34731ea36f31f7e
Doralitze <doralitze@chaotikum.org>
2023-04-04 13:52:05 +0200

    add: general parsing test

--- test/scheduler_test.cpp
+++ test/scheduler_test.cpp
@@ -2,8 +2,18 @@
 #define BOOST_TEST_MODULE RMRF_TESTS
 #include <boost/test/included/unit_test.hpp>
 
-using namespace dmxfish::io;
+#include <memory>
 
-BOOST_AUTO_TEST_CASE(scheduler_test) {	
-	BOOST_CHECK(true);
+#include "executioners/scene_factory.hpp"
+
+using namespace dmxfish::filters;
+using namespace dmxfish::execution;
+
+BOOST_AUTO_TEST_CASE(scheduler_test) {
+	std::vector<scene> v;
+	std::unique_ptr<MissionDMX::ShowFile::BordConfiguration> bc = std::make_unique<MissionDMX::ShowFile::BordConfiguration>(std::string("test/test_bord_config.xml"));
+
+	populate_scene_vector(v, bc->scene());
+
+	BOOST_CHECK_EQUAL(v.size(), 2);
 }
