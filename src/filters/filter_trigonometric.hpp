04f52c9cbb071dabc548ad8b23a3f327b20cf243
Ludwig Rahlff <ludwig.rahlff@student.uni-luebeck.de>
2023-04-27 09:02:55 +0200

    fix: trignometric bug, added square, triangle, sawtooth filter

--- src/filters/filter_trigonometric.hpp
+++ src/filters/filter_trigonometric.hpp
@@ -15,13 +15,22 @@ namespace dmxfish::filters {
 
     COMPILER_SUPRESS("-Weffc++")
     double sin_deg(double v, double f, double m, double p){
-        return std::sin(f*(v+p)*M_PI*m/180);
+        return f*std::sin(std::remainder((v+p)*m, 360.0)*M_PI/180);
     }
     double cos_deg(double v, double f, double m, double p){
-        return std::cos(f*(v+p)*M_PI*m/180);
+        return f*std::cos(std::remainder((v+p)*m, 360.0)*M_PI/180);
     }
     double tan_deg(double v, double f, double m, double p){
-        return std::tan(f*(v+p)*M_PI*m/180);
+        return f*std::tan(std::remainder((v+p)*m, 360.0)*M_PI/180);
+    }
+    double triangle(double v, double f, double m, double p){
+        return 1.0-2.0*std::abs(std::remainder((v+p-90.0)*m, 360.0)/180.0);
+    }
+    double sawtooth(double v, double f, double m, double p){
+        return f*std::remainder((v+p)*m, 360.0)/180.0;
+    }
+    double square(double v, double f, double m, double p, double l){
+        return f*(2.0*(std::remainder((v+p)*m, 360.0) > (l-180.0))-1);
     }
 
     double asin_deg(double v){
@@ -83,19 +92,88 @@ namespace dmxfish::filters {
         }
 
         virtual void update() override {
-            this->output = (*input);
+            this->output = F(*input, *factor_outer, *factor_inner, *phase);
         }
 
         virtual void scene_activated() override {}
 
     };
 
+    template <double (*F)(double, double, double, double, double)>
+    class filter_five_params: public filter {
+    private:
+        const double one = 1;
+        const double zero = 0;
+        const double half_circle = 180;
+        double* input = nullptr;
+        double* factor_outer = nullptr;
+        double* factor_inner = nullptr;
+        double* phase = nullptr;
+        double* length = nullptr;
+        double output = 0;
+    public:
+        filter_five_params() : filter() {}
+        virtual ~filter_five_params() {}
+
+        virtual void setup_filter(const std::map<std::string, std::string>& configuration, const std::map<std::string, std::string>& initial_parameters, const channel_mapping& input_channels) override {
+            MARK_UNUSED(initial_parameters);
+            MARK_UNUSED(configuration);
+            if(!input_channels.float_channels.contains("value")) {
+                throw filter_config_exception("Unable to link input of trigonometric filter: channel mapping does not contain channel 'value' of type 'double'.");
+            }
+            this->input = input_channels.float_channels.at("value");
+            if(input_channels.float_channels.contains("factor_outer")) {
+                this->factor_outer = input_channels.float_channels.at("factor_outer");
+            } else {
+                this->factor_outer = (double*) &this->one;
+            }
+            if(input_channels.float_channels.contains("factor_inner")) {
+                this->factor_inner = input_channels.float_channels.at("factor_inner");
+            } else {
+                this->factor_inner = (double*) &this->one;
+            }
+            if(input_channels.float_channels.contains("phase")) {
+                this->phase = input_channels.float_channels.at("phase");
+            } else {
+                this->phase = (double*) &this->zero;
+            }
+            if(input_channels.float_channels.contains("length")) {
+                this->length = input_channels.float_channels.at("length");
+            } else {
+                this->length = (double*) &this->half_circle;
+            }
+        }
+
+        virtual bool receive_update_from_gui(const std::string& key, const std::string& _value) override {
+            MARK_UNUSED(key);
+            MARK_UNUSED(_value);
+            return false;
+        }
+
+        virtual void get_output_channels(channel_mapping& map, const std::string& name) override {
+            map.float_channels[name + ":value"] = &output;
+        }
+
+        virtual void update() override {
+            this->output = F(*input, *factor_outer, *factor_inner, *phase, *length);
+        }
+
+        virtual void scene_activated() override {}
+
+    };
+
+
     using filter_sine = filter_trigonometric<sin_deg>;
     using filter_cosine = filter_trigonometric<cos_deg>;
     using filter_tangent = filter_trigonometric<tan_deg>;
+
     using filter_arcsine = filter_math_single<asin_deg>;
     using filter_arccosine = filter_math_single<acos_deg>;
     using filter_arctangent = filter_math_single<atan_deg>;
+
+    using filter_square = filter_five_params<square>;
+    using filter_triangle = filter_trigonometric<triangle>;
+    using filter_sawtooth = filter_trigonometric<sawtooth>;
 
     COMPILER_RESTORE("-Weffc++")
 }
