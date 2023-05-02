cdef8bc91db131ff0dec4238810e2c6dc7c4e059
Ludwig Rahlff <ludwig.rahlff@student.uni-luebeck.de>
2023-05-02 08:30:40 +0200

    fix: time generator

--- src/global_vars.hpp
+++ src/global_vars.hpp
@@ -2,12 +2,6 @@
 
 #include <chrono>
 
-static std::chrono::time_point<std::chrono::system_clock> start_time = std::chrono::system_clock::now();
+std::chrono::time_point<std::chrono::system_clock> get_start_time();
 
-std::chrono::time_point<std::chrono::system_clock> get_start_time(){
-    return start_time;
-}
-
-void reset_start_time(){
-    start_time = std::chrono::system_clock::now();
-}
+void reset_start_time();
