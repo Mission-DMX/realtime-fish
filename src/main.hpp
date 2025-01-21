a5e64cccad3ef907faf38aed4b1b7e4f497e26de
Leon Dietrich <doralitze@chaotikum.org>
2025-01-21 10:01:24 +0100

    chg: name of manger creation method

--- src/main.hpp
+++ src/main.hpp
@@ -8,7 +8,7 @@
 std::shared_ptr<dmxfish::io::IOManager> get_iomanager_instance();
 
 // only for testing purposes (and used in main_loop(), but only there!)
-void construct_iomanager();
-void destruct_iomanager();
+void construct_managers();
+void destruct_managers();
 
 std::shared_ptr<dmxfish::events::event_storage> get_event_storage_instance();
