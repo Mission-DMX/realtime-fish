d2f45b9a032339b2a38c47b684dd8c98c6847cd9
Doralitze <doralitze@chaotikum.org>
2025-02-05 18:12:13 +0100

    add: single input test case

--- test/color_mixing_test.cpp
+++ test/color_mixing_test.cpp
@@ -154,4 +154,50 @@ BOOST_AUTO_TEST_CASE(test_color_mix_filter_two_inputs) {
         test_zero_input_filter(cmf_nrgb);
 }
 
-// TODO also write test cases for 1 and 3
+void test_single_input_filter(filter& cmf) {
+    channel_mapping input_channels, output_channels;
+    std::map<std::string, std::string> configuration, initial_parameters;
+    dmxfish::dmx::pixel p;
+
+    configuration["input_count"] = "1";
+    input_channels.color_channels["0"] = &p;
+
+    cmf.setup_filter(configuration, initial_parameters, input_channels, "test_filter");
+    cmf.get_output_channels(output_channels, "test_filter");
+    cmf.scene_activated();
+
+    p.setHue(320.0);
+    p.setSaturation(1.0);
+    p.setIluminance(0.5);
+
+    cmf.update();
+
+    const auto h = output_channels.color_channels["test_filter:value"]->getHue();
+    BOOST_TEST(h > 319.5, "Expected hue to be approximately 45 deg. Actual: " + std::to_string(h));
+    BOOST_TEST(h < 320.5, "Expected hue to be approximately 45 deg. Actual: " + std::to_string(h));
+    const auto s = output_channels.color_channels["test_filter:value"]->getSaturation();
+    BOOST_TEST(s > 0.95, "Expected saturation to be approximately 1. Actual: " + std::to_string(s));
+    BOOST_TEST(s < 1.05, "Expected saturation to be approximately 1. Actual: " + std::to_string(s));
+    const auto i = output_channels.color_channels["test_filter:value"]->getIluminance();
+    BOOST_TEST(i > 0.45, "Expected intensity to be approximately 1. Actual: " + std::to_string(i));
+    BOOST_TEST(i < 0.55, "Expected intensity to be approximately 1. Actual: " + std::to_string(i));
+}
+
+BOOST_AUTO_TEST_CASE(test_color_mix_filter_single_input) {
+        spdlog::set_level(spdlog::level::info);
+        //spdlog::set_level(spdlog::level::debug);
+
+        spdlog::info("Testing CSV mixing with 0 inputs");
+        filter_color_mixer_hsv cmf_hsv;
+        test_single_input_filter(cmf_hsv);
+
+        spdlog::info("Testing RGB adding with 0 inputs");
+        filter_color_mixer_add_rgb cmf_argb;
+        test_single_input_filter(cmf_argb);
+
+        spdlog::info("Testing RGB normative mixing with 0 inputs");
+        filter_color_mixer_norm_rgb cmf_nrgb;
+        test_single_input_filter(cmf_nrgb);
+}
+
+// TODO also write test case for 3
