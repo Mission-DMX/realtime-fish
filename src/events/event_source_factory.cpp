618f668e79c98a8e071da287818132ade4ee1f10
Leon Dietrich <doralitze@chaotikum.org>
2025-03-31 16:44:38 +0200

    add: remote event_source creation

--- src/events/event_source_factory.cpp
+++ src/events/event_source_factory.cpp
@@ -15,8 +15,14 @@ namespace dmxfish::events {
         auto storage_ptr = get_event_storage_instance();
         if(const auto s_ptr = storage_ptr->find_source_by_name(msg.name()); s_ptr != nullptr) {
             return s_ptr->update_conf_from_message(msg);
-        } else {
-            // TODO construct sender
         }
+        const auto& type = msg.type();
+        if (type == "fish.builtin.plain") {
+            return event_source::create<event_source>(storage_ptr, msg.name())->update_conf_from_message(msg);
+        }
+        // TODO develop event_source fish.builtin.midi
+        // TODO develop event_source fish.builtin.midirtp
+        // TODO develop event_source fish.builtin.gpio
+        // TODO develop event_source fish.builtin.macrokeypad
     }
 }
