1963f218f9a58726fac9a448a69d98001023d0be
Doralitze <doralitze@chaotikum.org>
2025-05-13 12:11:25 +0200

    add: event insertion

--- src/sound/audioinput_event_source.cpp
+++ src/sound/audioinput_event_source.cpp
@@ -5,18 +5,29 @@
 
 #include "audioinput_event_source.hpp"
 
+#include <array>
 #include <Eigen/Dense>
 
+#include "event/event_storage.hpp"
 #include "sound/ALSA/ALSA.H"
 #include "sound/ALSA/Capture.H"
 
-
 #include "lib/logging.hpp"
+#include "main.hpp"
 
 namespace dmxfish::audio {
 
+    constexpr auto fft_size = 1024;
+
     audioinput_event_source::audioinput_event_source() {
 
+    }
+
+    audioinput_event_source::~audioinput_event_source() {
+        this->running = false;
+        if (this->thread.has_value()) {
+            this->thread->join();
+        }
     }
 
     missiondmx::fish::ipcmessages::event_sender audioinput_event_source::encode_proto_message() const {
@@ -29,9 +40,12 @@ namespace dmxfish::audio {
     }
 
     bool audioinput_event_source::update_conf_from_message(const missiondmx::fish::ipcmessages::event_sender& msg) {
-        // TODO update cutoff settings
+        // TODO update cutoff and magnitude settings
         auto conf = msg.configuration();
-        if (conf["dev"] == this->sound_dev_file) {
+        if (conf["dev"] == this->sound_dev_file
+                && channel_count == std::stoi(conf["channel_count"])
+                && sampler_rate == std::stoi(conf["sampler_rate"])
+                && record_block_duration_ms == std::stoi(conf["sample_duration"])) {
             return true;
         }
         this->running = false;
@@ -44,6 +58,16 @@ namespace dmxfish::audio {
 
     void audioinput_event_source::update_task() {
         using namespace ALSA;
+
+        if(!this->running) {
+            return;
+        }
+
+        if(this->channel_count < 1) {
+            ::spdlog::error("Beat analysis requires at least 1 input channel. {} were configured.", this->channel_count);
+            return;
+        }
+
         Capture capture_dev(this->sound_dev_file.c_str());
         if (capture_dev.prepared()) {
             ::spdlog::error("Failed to open sound input device {}", this->sound_dev_file);
@@ -77,12 +101,69 @@ namespace dmxfish::audio {
         snd_pcm_uframes_t pSize;
         capture_dev.getPeriodSize(&pSize);
 
-        ::spdlog::info("Successfully opened device {} for capturing audio with format {}, {} channels, period size {} and a sampler rate of {}.",
+        ::spdlog::info("Successfully initialized device {} for capturing audio with format {}, {} channels, period size {} and a sampler rate of {}.",
                        this->sound_dev_file, capture_dev.getFormatName(format), capture_dev.getChannels(), pSize, this->sampler_rate);
+        std::array<double, fft_size> fft_buffer;
+        size_t initial_fft_buffer_pos = 0;
+
+        auto event_storage = get_event_storage_instance();
 
         while(this->running) {
             capture_dev >> buffer;
-            // TODO analyze input
+            if (buffer.rows() == 0) {
+                continue;
+            }
+            // A better approach than this is http://werner.yellowcouch.org/Papers/bpm04/
+            // However implementing this on roalling data needs some math which I don't have
+            // the time for at the moment. Therefore FFT it is.
+
+            remaining_elements_in_buffer = buffer.rows();
+            while (remaining_elements_in_buffer > fft_buffer.size()) {
+                // load buffer content
+                for (auto i = initial_fft_buffer_pos; i < fft_buffer.size(); i++) {
+                    double avg = 0;
+                    for (auto j = 0; j < buffer.cols(); j++) {
+                        avg += buffer[buffer.rows() - remaining_elements_in_buffer][j];
+                    }
+                    fft_buffer[i] = avg / buffer.cols();
+                    remaining_elements_in_buffer--;
+                }
+                initial_fft_buffer_pos = 0;
+
+                // analyze buffer
+                std::array<double, fft_size> real, imag;
+                fft(fft_buffer, real, imag);
+
+                for (auto i=0; i < real.size(); i++) {
+                    real[i] = real[i] * real[i];
+                }
+                for (auto i=0; i < imag.size(); i++) {
+                    imag[i] = imag[i] * imag[i];
+                }
+
+                double bassIntensity = 0;
+                for (i=this->low_cutoff_frequency; i < this->high_cutoff_frequency; i++){
+                    bassIntensity += real[i] + imag[i];
+                }
+                if (bassIntensity > this->trigger_magnitude) {
+                    dmxfish::events::event e(this->get_sender_id(), 0);
+                    event_storage->insert_event(e);
+                }
+            }
+
+            // preload buffer for next iteration and update initial_fft_buffer_pos
+            while(remaining_elements_in_buffer > 0) {
+                double avg = 0;
+                for (auto j = 0; j < buffer.cols(); j++) {
+                    avg += buffer[buffer.rows() - remaining_elements_in_buffer][j];
+                    remaining_elements_in_buffer--;
+                }
+                fft_buffer[initial_fft_buffer_pos++] = avg / buffer.cols();
+            }
         }
+    }
+
+    void fft(const std::array<double, fft_size>& in_buffer, std::array<double, fft_size>& real, std::array<double, fft_size>& imag) {
+        // TODO
     }
 }
--- src/sound/audioinput_event_source.hpp
+++ src/sound/audioinput_event_source.hpp
@@ -16,15 +16,20 @@ namespace dmxfish::audio {
 
     class audioinput_event_source : public dmxfish::events::event_source {
     private:
-        // TODO implement atomic cutoff settings
         std::atomic_bool running = false;
-        std::atomic_short channel_count = 1;
-        std::atomic_int sampler_rate = 48000;
-        std::atomic_int record_block_duration_ms = 2100;
+        std::atomic_int high_cutoff_frequency = 96;
+        std::atomic_int low_cutoff_frequency = 9;
+        std::atomic_double trigger_magnitude = 30.0;
+
+        unsigned short channel_count = 1;
+        unsigned short sampler_rate = 48000;
+        unsigned short record_block_duration_ms = 40;
+
         std::string sound_dev_file = "";
         std::optional<std::thread> thread;
     public:
         audioinput_event_source();
+        ~audioinput_event_source();
         [[nodiscard]] missiondmx::fish::ipcmessages::event_sender encode_proto_message() const override;
         [[nodiscard]] bool update_conf_from_message(const missiondmx::fish::ipcmessages::event_sender& msg) override;
     private:
