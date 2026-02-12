7dda35ef0040e1e145f30b716f619f3cb3989fba
Leon Dietrich <doralitze@chaotikum.org>
2026-02-12 21:44:38 +0100

    add: get_own_scene_id() lua function

--- src/filters/lua/lua_scene_api.cpp
+++ src/filters/lua/lua_scene_api.cpp
@@ -15,23 +15,37 @@
 
 namespace dmxfish::filters::lua {
     size_t get_scene_count() {
-	if (auto io_mgr_ptr = get_iomanager_instance(); io_mgr_ptr != nullptr) [[likely]] {
-	    if (auto show_ptr = io_mgr_ptr->get_active_show(); show_ptr != nullptr) [[likely]] {
-		return show_ptr->get_scene_count();
-	    } else {
-		return 0;
-	    }
-	} else {
-	    return 0;
-	}
+        if (auto io_mgr_ptr = get_iomanager_instance(); io_mgr_ptr != nullptr) [[likely]] {
+            if (auto show_ptr = io_mgr_ptr->get_active_show(); show_ptr != nullptr) [[likely]] {
+                return show_ptr->get_scene_count();
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    size_t get_current_active_scene() {
+        if (auto io_mgr_ptr = get_iomanager_instance(); io_mgr_ptr != nullptr) [[likely]] {
+            if (auto show_ptr = io_mgr_ptr->get_active_show(); show_ptr != nullptr) [[likely]] {
+                return show_ptr->get_active_scene_index();
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
     }
 
     void init_lua_scene_api(sol::state& lua) {
-	lua.set_function("get_scene_count",
-		dmxfish::filters::lua::get_scene_count
-		);
+        lua.set_function("get_scene_count",
+		    dmxfish::filters::lua::get_scene_count
+        );
+        lua.set_function("get_own_scene_id",
+            dmxfish::filters::lua::get_current_active_scene
+        );
 
-	// get_own_scene_id()
 	// switch_to_scene()
     }
 }
